97d3d3b751305f3c2ba1afe985a4126312e841d5
zhangqiang <zhangqianggis@sina.com>
2017-11-19 11:51:11 +0800

    frm 110

--- CNNmodel2.py
+++ CNNmodel2.py
@@ -9,9 +9,9 @@ import tensorflow as tf
 import numpy as np
 
 from common_fun import print_info
-from read_CNN_data import read_all_train_data,read_test_data
+from read_CNN_data import read_all_train_data,read_test_data,read_train_data_split
 from savetoimage import save_viaris_img_2
-step_offset = 20
+step_offset = 180
 def rot_data(x_data,idx):
     x_0 = x_data[idx]
     x_0_90 = x_0
@@ -22,7 +22,8 @@ def rot_data(x_data,idx):
         x_0_180[:,:,i] = np.rot90(x_0_180[:,:,i],2)
         x_0_270[:,:,i] = np.rot90(x_0_270[:,:,i],3)
     return x_0,x_0_90,x_0_180,x_0_270
-def get_next_batch_2(x_data,y_data,batch_size = 32):
+
+def get_next_batch_2(x_data,y_data,batch_size = 64):
     x = []
     y = []
     for _ in range(batch_size):
@@ -54,7 +55,7 @@ def get_next_batch(x_data,y_data,batch_size = 100):
     x = []
     y = []
     for _ in range(batch_size // 20):
-        idx = np.random.randint(0,50)
+        idx = np.random.randint(0,step_offset)
         x_0,x_9,x_18,x_27 = rot_data(x_data,idx)
             
         x.append(x_0)
@@ -67,7 +68,7 @@ def get_next_batch(x_data,y_data,batch_size = 100):
         y.append(y_data[idx])
         y.append(y_data[idx])
         
-        idx = idx + 50
+        idx = idx + step_offset
         x_0,x_9,x_18,x_27 = rot_data(x_data,idx)
             
         x.append(x_0)
@@ -80,7 +81,7 @@ def get_next_batch(x_data,y_data,batch_size = 100):
         y.append(y_data[idx])
         y.append(y_data[idx])
         
-        idx = idx + 50 
+        idx = idx + step_offset 
         x_0,x_9,x_18,x_27 = rot_data(x_data,idx)
             
         x.append(x_0)
@@ -93,7 +94,7 @@ def get_next_batch(x_data,y_data,batch_size = 100):
         y.append(y_data[idx])
         y.append(y_data[idx])
         
-        idx = idx + 50 
+        idx = idx + step_offset 
         x_0,x_9,x_18,x_27 = rot_data(x_data,idx)
             
         x.append(x_0)
@@ -106,7 +107,7 @@ def get_next_batch(x_data,y_data,batch_size = 100):
         y.append(y_data[idx])
         y.append(y_data[idx])
         
-        idx = idx + 50 
+        idx = idx + step_offset 
         x_0,x_9,x_18,x_27 = rot_data(x_data,idx)
             
         x.append(x_0)
@@ -132,7 +133,7 @@ def get_next_batch(x_data,y_data,batch_size = 100):
 #定义CNN
 def create_cnn(out_size,w_alpha = 0.01,b_alpha = 0.01):
     #x = tf.reshape(X,shape=[-1,15,15,360])
-    x = tf.reshape(X,shape=[-1,5,5,224])
+    x = tf.reshape(X,shape=[-1,9,9,224])
     #w_c1 = tf.Variable(w_alpha*tf.random_normal([3,3,360,512]))
     w_c1 = tf.Variable(w_alpha*tf.random_normal([3,3,224,512]))
     b_c1 = tf.Variable(b_alpha*tf.random_normal([512]))
@@ -153,8 +154,7 @@ def create_cnn(out_size,w_alpha = 0.01,b_alpha = 0.01):
     conv3 = tf.nn.dropout(conv3,keep_prob)
     print(conv3.get_shape().as_list())
     #fully connect layer
-    #w_d = tf.Variable(w_alpha*tf.random_normal([2*2*2048,2048]))
-    w_d = tf.Variable(w_alpha*tf.random_normal([1*1*2048,2048]))
+    w_d = tf.Variable(w_alpha*tf.random_normal([2*2*2048,2048]))
     b_d = tf.Variable(b_alpha*tf.random_normal([2048]))
     dense = tf.reshape(conv3,[-1,w_d.get_shape().as_list()[0]])
     print(dense.get_shape().as_list())
@@ -169,9 +169,42 @@ def create_cnn(out_size,w_alpha = 0.01,b_alpha = 0.01):
     
     return out
 
+def create_cnn_3muti3(out_size,w_alpha = 0.01,b_alpha = 0.01):
+    #x = tf.reshape(X,shape=[-1,15,15,360])
+    x = tf.reshape(X,shape=[-1,3,3,224])
+    #w_c1 = tf.Variable(w_alpha*tf.random_normal([3,3,360,512]))
+    w_c1 = tf.Variable(w_alpha*tf.random_normal([3,3,224,256]))
+    b_c1 = tf.Variable(b_alpha*tf.random_normal([256]))
+    conv1 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(x,w_c1,strides=[1,1,1,1],padding='SAME'),b_c1))
+    conv1 = tf.nn.max_pool(conv1,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
+    conv1 = tf.nn.dropout(conv1,keep_prob)
+    print(conv1.get_shape().as_list())
+    w_c2 = tf.Variable(w_alpha*tf.random_normal([3,3,256,512]))
+    b_c2 = tf.Variable(b_alpha*tf.random_normal([512]))
+    conv2 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(conv1,w_c2,strides=[1,1,1,1],padding='SAME'),b_c2))
+    conv2 = tf.nn.max_pool(conv2,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
+    conv2 = tf.nn.dropout(conv2,keep_prob)
+    print(conv2.get_shape().as_list())
+    
+    #fully connect layer
+    w_d = tf.Variable(w_alpha*tf.random_normal([1*1*512,1024]))
+    b_d = tf.Variable(b_alpha*tf.random_normal([1024]))
+    dense = tf.reshape(conv2,[-1,w_d.get_shape().as_list()[0]])
+    print(dense.get_shape().as_list())
+    dense = tf.nn.relu(tf.add(tf.matmul(dense,w_d),b_d))
+    dense = tf.nn.dropout(dense,keep_prob)
+    print(dense.get_shape().as_list())
+    
+    w_out = tf.Variable(w_alpha*tf.random_normal([1024,out_size]))
+    b_out = tf.Variable(b_alpha*tf.random_normal([out_size]))
+    out = tf.add(tf.matmul(dense,w_out),b_out)
+    print(out.get_shape().as_list())
+    
+    return out
+
 def train_cnn(x_all_data,y_all_data):
     #output = create_cnn(3)
-    output = create_cnn(5)
+    output = create_cnn_3muti3(5)
     
     print_info("created cnn ...")
     print_info("start train ...")
@@ -191,7 +224,7 @@ def train_cnn(x_all_data,y_all_data):
         
         step = 0
         while True:
-            batch_x,batch_y = get_next_batch_2(x_all_data,y_all_data)
+            batch_x,batch_y = get_next_batch(x_all_data,y_all_data)
             _,loss_ = sess.run([optimizer,loss],feed_dict = {X:batch_x,Y:batch_y,keep_prob:0.75})
             print(step,loss_)
             '''
@@ -205,22 +238,20 @@ def train_cnn(x_all_data,y_all_data):
             print("max_l",max_l)
             break
             '''
-            if step % 10 == 0 and step != 0:
-                batch_x_test,batch_y_test = get_next_batch_2(x_all_data,y_all_data)
+            if step % 10 == 0:
+                batch_x_test,batch_y_test = get_next_batch(x_all_data,y_all_data)
                 acc = sess.run(accuracy,feed_dict={X:batch_x_test,Y:batch_y_test,keep_prob:1.0})
-                print(step,acc)
-                if acc > 0.7:
-                    saver.save(sess,"./model/cnn.model",global_step=step)
-                if acc > 0.8:
-                    saver.save(sess,"./model/cnn.model",global_step=step)
+                print(step,acc)                
                     #break
                 if acc > 0.9:
-                    saver.save(sess,"./model/cnn.model",global_step=step)
+                    saver.save(sess,"./model_3/cnn.model",global_step=step)
+                if acc > 0.98:
+                    saver.save(sess,"./model_3/cnn.model",global_step=step)
                     break
             step += 1
 def test_cnn(x_data):
     
-    output = create_cnn(5)
+    output = create_cnn_3muti3(5)
     l = []
     saver = tf.train.Saver()
     #new_saver = tf.train.import_meta_graph("./model/cnn.model-470.meta") 
@@ -229,42 +260,47 @@ def test_cnn(x_data):
         
         #new_saver.restore(sess,tf.train.latest_checkpoint('./model/')) 
         #new_saver.restore(sess,"./model/cnn.model-470")                      
-        saver.restore(sess,"./model_100/cnn.model-1450")
+        saver.restore(sess,"./model_3/cnn.model-22500")
         #all_vars = tf.trainable_variables()
         #sess.run(tf.global_variables_initializer())
         preject = tf.argmax(output,1)
-        for i in range(len(x_data)//100):
-            x_in = x_data[i*100:(i+1)*100]
+        '''
+        x_in = np.array(x_data)
+        
+        label = sess.run(preject,feed_dict={X:x_in,keep_prob:1})
+        '''
+        for i in range(len(x_data)//398):
+            x_in = x_data[i*398:(i+1)*398]
             x_in = np.array(x_in)
             label = sess.run(preject,feed_dict={X:x_in,keep_prob:1})
             l.append(label)            
         #print(label)
+        
     return l
-
 if __name__ == '__main__':
     #
-    train = 1
+    train = 0
     if train == 1:
-        x_data,y_data = read_all_train_data()
+        #x_data,y_data = read_all_train_data()
+        x_data,y_data = read_train_data_split()
         #X = tf.placeholder(tf.float32,[84,15,15,360])
         #Y = tf.placeholder(tf.float32,[84,3])
         
-        X = tf.placeholder(tf.float32,[None,5,5,224])
+        X = tf.placeholder(tf.float32,[None,3,3,224])
         Y = tf.placeholder(tf.float32,[None,5])
         
         keep_prob = tf.placeholder(tf.float32)
         train_cnn(x_data,y_data)
     if train == 0:
-        x_data = read_test_data('E:/Imaging/CNNTest/test_108_aviris_3.txt')
+        x_data = read_test_data('E:/Imaging/ROI_AVIRIS_test/test_roi_2_frm_envi.txt',3)
         #x_in = x_data[:24]
         #x_in = np.array(x_in)
         tf.reset_default_graph()  
-        X = tf.placeholder(tf.float32,[None,5,5,224])
+        X = tf.placeholder(tf.float32,[None,3,3,224])
         #Y = tf.placeholder(tf.float32,[24,3])
         keep_prob = tf.placeholder(tf.float32)
         l = test_cnn(x_data)
-        a = l
-        a = np.array(a)
-        a = a.reshape(1,10000)
-
-        save_viaris_img_2(a,'cnn1450.jpg',100,100)
+        
+        a = np.array(l)
+        a = a.reshape(1,158404)
+        save_viaris_img_2(a,'test_roi_2_cnn.jpg',398,398)
